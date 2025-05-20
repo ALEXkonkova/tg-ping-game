81a4e35905b7e41891e813e169964d786706753d
ALEXkonkova <73539963+ALEXkonkova@users.noreply.github.com>
2025-05-20 03:28:35 +0300

    Add files via upload

---
+++ bot_main.py
@@ -0,0 +1,34 @@
+# bot_main.py
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+BOT_TOKEN = "7031125260:AAHWfwygmsCXWojp8g3A3DD708kT-f15s8c"  # вставь сюда свой токен
+
+WEB_APP_URL = "https://alexkonkova.github.io/tg-ping-game/"
+
+#WEB_APP_URL = "http://localhost:5000"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    keyboard = [
+        [InlineKeyboardButton("🎮 Играть в Пинг-Понг", web_app=WebAppInfo(url=WEB_APP_URL))]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text("Привет! Нажми кнопку, чтобы играть в Пинг-Понг!", reply_markup=reply_markup)
+
+def run_bot():
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    print("Бот запущен.")
+    app.run_polling()
+
+if __name__ == "__main__":
+    from multiprocessing import Process
+    from local_webapp import run_flask_app
+
+    flask_process = Process(target=run_flask_app)
+    flask_process.start()
+
+    try:
+        run_bot()
+    finally:
+        flask_process.terminate()
